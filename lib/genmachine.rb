68c9277835dcdd34e27de97aa38a1ada2712a4c8
Joseph Wecker <joseph.wecker@gmail.com>
2011-08-11 10:38:54 -0700

    Trying to autodiscover the target languages available

--- lib/genmachine.rb
+++ lib/genmachine.rb
@@ -1,2 +1,14 @@
-#require 'genmachine'
+require 'genmachine/spec_parser'
+require 'genmachine/generator'
+require 'genmachine/generators/helpers/general'
 
+Dir[File.join(File.dirname(__FILE__),'genmachine','generators','*.rb')].each do |fname|
+  name = File.basename(fname)
+  require "genmachine/generators/#{name}"
+end
+
+module GenMachine
+  def languages
+    GenMachine::Generators.constants.map{|const| underscore(const)}
+  end
+end
