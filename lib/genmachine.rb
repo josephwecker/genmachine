4980dd63592ea44a2278329790edadbc07692948
Joseph Wecker <joseph.wecker@gmail.com>
2011-08-10 18:53:15 -0700

    Going to separate out the spec parser from the actual generator

--- lib/genmachine.rb
+++ lib/genmachine.rb
@@ -1,8 +1,14 @@
 module GenMachine
-  class Builder
+  class SpecParser
     def initialize(files)
       @files = files
       @table = []
+    end
+  end
+
+  class Builder
+    def initialize
+
     end
   end
 end
