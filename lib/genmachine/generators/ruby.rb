83b3ce9ae280bfc3de05e56c55cacabbf5fb8236
Joseph Wecker <joseph.wecker@gmail.com>
2011-08-16 14:21:15 -0700

    Honor -e flag and correct output directory for ruby parsers

--- lib/genmachine/generators/ruby.rb
+++ lib/genmachine/generators/ruby.rb
@@ -15,14 +15,15 @@ module GenMachine
 
       def generate_class
         library = ERB.new(IO.read(@template_base+'lib.erb.rb'),nil,'-')
-        f = File.new(@class_fname, 'w+')
+        f = File.new(File.join(@output_dir,@class_fname), 'w+')
         f.write(library.result(binding))
         f.close
       end
 
       def generate_executable
+        return unless @gen_executable
         executable = ERB.new(IO.read(@template_base+'executable.erb'),nil,'-')
-        f = File.new(@exe_fname, 'w+')
+        f = File.new(File.join(@output_dir,@exe_fname), 'w+')
         f.write(executable.result(binding))
         f.chmod(0755)
         f.close
